69fb687f61f8f827fa4453b0ce79d61a344e0350
rhithesh <rhithesh1947@gmail.com>
2025-01-26 14:48:01 +0530

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Days from './pages/Days';
 import { db } from './db';
@@ -12,36 +12,33 @@ function App() {
   };
 
   useEffect(() => {
-  
     checkCompletedDays();
   }, []);
 
   return (
-    <div className='bg-black text-white min-h-screen p-8'>
-      <div className='max-w-6xl  flex gap-10'>
-        <div className='w-full mx-auto'>
-          <div className='container mb-10'>
-            <h1 className='text-6xl pb-1.5 font-bold text-white'>
-              120 days challenge
-            </h1>
-            <div className='text-xl mt-2'>
-              Completed Days: {completedDays}/120
-            </div>
-            <button 
-              className='mt-4 text-2xl bg-gray-800 border-2 border-white rounded-2xl px-4 py-2 cursor-pointer hover:bg-gray-700 transition-colors' 
-              onClick={() => {
-                db.challenge.clear();
-                window.location.reload();
-              }}
-            >
-              Reset Challenge
-            </button>
+    <div className='bg-black text-white min-h-screen flex justify-center items-center p-8'>
+      <div className='w-full max-w-[1280px]'>
+        <div className='container text-center mb-10'>
+          <h1 className='text-6xl pb-1.5 font-bold text-white'>
+            120 days challenge
+          </h1>
+          <div className='text-xl mt-2'>
+            Completed Days: {completedDays}/120
           </div>
-          <div className='bg-gray-900 w-[1280px] border-2 border-gray-700 rounded-xl p-6 flex gap-12 flex-wrap'>
-            {Array(120).fill(0).map((_, index) => (
-              <Days f={checkCompletedDays} key={index} day={index + 1} />
-            ))}
-          </div>
+          <button
+            className='mt-4 text-2xl bg-gray-800 border-2 border-white rounded-2xl px-4 py-2 cursor-pointer hover:bg-gray-700 transition-colors'
+            onClick={() => {
+              db.challenge.clear();
+              window.location.reload();
+            }}
+          >
+            Reset Challenge
+          </button>
+        </div>
+        <div className='bg-gray-900 border-2 border-gray-700 rounded-xl p-6 flex justify-center gap-12 flex-wrap'>
+          {Array(120).fill(0).map((_, index) => (
+            <Days f={checkCompletedDays} key={index} day={index + 1} />
+          ))}
         </div>
       </div>
     </div>
