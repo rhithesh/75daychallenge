c5ce8e295a21bf358ff0235808d0a3c6f7572ce8
rhithesh <rhithesh1947@gmail.com>
2025-01-26 14:36:24 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ function App() {
 
   return (
     <div className='bg-black text-white min-h-screen p-8'>
-      <div className='max-w-6xl mx-auto flex gap-10'>
-        <div className='w-full'>
+      <div className='max-w-6xl  flex gap-10'>
+        <div className='w-full mx-auto'>
           <div className='container mb-10'>
             <h1 className='text-6xl pb-1.5 font-bold text-white'>
               120 days challenge
@@ -37,7 +37,7 @@ function App() {
               Reset Challenge
             </button>
           </div>
-          <div className='bg-gray-900 border-2 border-gray-700 rounded-xl p-6 flex gap-12 flex-wrap'>
+          <div className='bg-gray-900 w-[1280px] border-2 border-gray-700 rounded-xl p-6 flex gap-12 flex-wrap'>
             {Array(120).fill(0).map((_, index) => (
               <Days f={checkCompletedDays} key={index} day={index + 1} />
             ))}
